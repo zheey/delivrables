479462902781143a6dddf397598d538f62484734
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-06-01 22:06:48 +0100

    added background image

--- src/landing-page/LandingPage.tsx
+++ src/landing-page/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import Navbar from "../common/Navbar";
+import LandingHeader from "./LandingHeader";
 
 const LandingPage: FC = () => {
     return (
         <div className="landing-page-wrapper">
             <Navbar/>
+            <LandingHeader/>
         </div>
     )
 };
