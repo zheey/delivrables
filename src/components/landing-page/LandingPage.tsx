a817f119025c5c85d6269e527f0e811c1a97ab22
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-06-03 23:54:15 +0100

    added subscribe section

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import Navbar from "../common/Navbar";
 import LandingHeader from "./LandingHeader";
+import SubscribeSection from "./SubcribeSection";
 
 const LandingPage: FC = () => {
     return (
         <div className="landing-page-wrapper">
             <Navbar/>
             <LandingHeader/>
+            <SubscribeSection/>
         </div>
     )
 };
---
+++ src/components/landing-page/SubcribeSection.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { TextHeader } from '../common/Text';
+
+const SubscribeSection: FC = () => {
+    return(
+        <div className="subscribe-section">
+            <TextHeader content="25% OFF on your first order by subscribing to our newsletters!"/>
+
+        </div>
+    )
+};
+
+export default SubscribeSection;
