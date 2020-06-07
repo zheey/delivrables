acd028e8deb329c9b5f901484144de3f8fa43c4e
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-06-07 23:04:52 +0100

    added login form input

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
-import { TextHeader } from "../common/Text";
+import { TextHeader, TextPargraph } from "../common/Text";
+import { NakedButton } from "../common/Button";
+import InputWrapper from "../common/InputWrapper";
 
 const Login:FC = () => {
     return(
@@ -9,6 +11,17 @@ const Login:FC = () => {
                 </div>
                 <div className="login-form-wrapper">
                     <TextHeader content="Welcome Back!"/>
+                    <TextPargraph content="Enter your details to login to your account"/>
+                    <NakedButton label="Sign In with Facebook"/>
+                    <TextPargraph content="Or sign in with e-mail:" className="small-text"/>
+                    <InputWrapper className="mb-26">
+                        <TextHeader content="Your Email"/>
+                        <input placeholder="assuranceuwanguezz@gmail.com" type="text"/>
+                    </InputWrapper>
+                    <InputWrapper>
+                        <TextHeader content="Your password"/>
+                        <input placeholder="assuranceuwanguezz@gmail.com" type="password"/>
+                    </InputWrapper>
                 </div>
             </div>
         </div>
