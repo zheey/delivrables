039c746491d268b03db074285bab9cd0e4ec999d
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-06-10 07:54:51 +0100

    created landing page wrapper component

---
+++ src/components/common/LandingPageWrapper.tsx
@@ -0,0 +1,57 @@
+import React, { FC, useState } from "react";
+import Navbar from "../common/Navbar";
+import SubscribeSection from "../landing-page/SubcribeSection";
+import Footer from "../landing-page/Footer";
+import Login from "../login/Login";
+import { ILandingState } from "../../interfaces/componentsStates";
+import SignUp from "../login/SignUp";
+import { ILandingPageWrapper } from "../../interfaces/componentProps";
+
+const LandingPageWrapper: FC<ILandingPageWrapper> = ({ children }) => {
+    const [landingPageState, setLandingPage] = useState<ILandingState>({
+        showLoginDrawer: false,
+        showSignupDrawer: false
+    });
+
+    const toogleLoginModal: () => void = () => {
+        setLandingPage(prevState => ({
+            ...prevState,
+            showSignupDrawer: false,
+            showLoginDrawer: !prevState.showLoginDrawer
+        }));
+    };
+
+    const toogleSignupModal: () => void = () => {
+        setLandingPage(prevState => ({
+            ...prevState,
+            showLoginDrawer: false,
+            showSignupDrawer: !prevState.showSignupDrawer
+        }));
+    };
+
+    const showDrawer = landingPageState.showLoginDrawer || landingPageState.showSignupDrawer;
+    return (
+        <div className={`landing-page-wrapper ${showDrawer ? "vh-sized" : ''}`}>
+            <Navbar onLoginClick={toogleLoginModal} onSignupClick={toogleSignupModal}/>
+            {
+                children
+            }
+            <SubscribeSection/>
+            <Footer/>
+            {
+                landingPageState.showLoginDrawer ? 
+                    <Login onClick={toogleLoginModal} onSignUp={toogleSignupModal}/>
+                    :
+                    ''
+            }
+            {
+                landingPageState.showSignupDrawer ? 
+                    <SignUp onClick={toogleSignupModal} onLogin={toogleLoginModal}/>
+                    :
+                    ''
+            }
+        </div>
+    )
+};
+
+export default LandingPageWrapper;
