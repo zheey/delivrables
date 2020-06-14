1b2d5a1e56ea303d87aea595c54619a881150364
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-06-14 17:20:44 +0100

    added overlay-bg

---
+++ src/components/common/MenuDetailsOverlay.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const MenuDetailsOverlay:FC = () => {
+    return(
+        <div className="login-wrapper overlay-wrapper">
+            <div>
+
+            </div>
+        </div>
+    )
+};
+
+export default MenuDetailsOverlay;
