b15bc9c49904bb4f88c93531f554baa925355dc9
Zainab Adegoke <zainabolaitan5@gmail.com>
2020-06-22 22:18:48 +0100

    added menu text content

--- src/components/AllMenuComponent.tsx
+++ src/components/AllMenuComponent.tsx
@@ -4,6 +4,9 @@ import RecentMenu from "./common/RecentMenu";
 import PopularMenu from "./common/PopularMenu";
 import { IAllMenu } from "../interfaces/componentsStates";
 import { IMenu } from "../interfaces/componentProps";
+import ImageWrapper from "./common/ImageWrapper";
+import imageDetails from "../images/menuDetails.png";
+import { TextHeader, TextPargraph } from "./common/Text";
 
 const AllMenuComponent: FC<IMenu> = () => {
     const [menuPageState, setMenuPage] = useState<IAllMenu>({
@@ -25,7 +28,15 @@ const AllMenuComponent: FC<IMenu> = () => {
                 menuPageState.showMenu ?
                     <div className="login-wrapper overlay-wrapper">
                         <div className="menu-details-card">
-
+                            <ImageWrapper image={imageDetails} alt="menu"/>
+                            <div className="menu-text-wrapper">
+                                <TextHeader content="Buttermilk Fried Chicken Wrap"/>
+                                <TextPargraph content="Allergies, intolerances and dietary requirements: before ordering, please contact the restaurant directly and ask to speak to the Manager who can help cater for your needs. For more information please see Costa."/>
+                            </div>
+                            <div className="sub-header">
+                                <TextHeader content="Add-ons"/>
+                                <TextPargraph content="Add some stuff to spice up your meal"/>
+                            </div>
                         </div>
                     </div>
                     :
